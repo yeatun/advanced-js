c77e6a66b3deac23be6608611ae93c237246f223
yeatun <p.yeatun@gmail.com>
2021-02-02 12:57:17 +0600

    adding double-vs-tripple-equal.js

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,45 @@
+// checking for ==
+const first =2;
+const second = 2 ;
+if(first == second)
+{
+    console.log("condition is true");
+
+}
+else {
+    console.log ("condition is false");
+}
+// 2
+const first =0;
+const second = false ;
+if(first == second)
+{
+    console.log("condition is true");
+
+}
+else {
+    console.log ("condition is false");
+}
+
+// checking for ===
+const first =2;
+const second = 2 ;
+if(first === second)
+{
+    console.log("condition is true");
+
+}
+else {
+    console.log ("condition is false");
+}
+// 2
+const first =2;
+const second =false ;
+if(first === second)
+{
+    console.log("condition is true");
+
+}
+else {
+    console.log ("condition is false");
+}
